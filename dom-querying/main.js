011009f622d6f263c3d3ce12367b2e1da4d4d6dc
Jeff Jin <jeffmjin@gmail.com>
2022-10-24 16:27:55 +0000

    Completed dom-querying exercise.

---
+++ dom-querying/main.js
@@ -0,0 +1,17 @@
+var h1 = document.querySelector('h1');
+console.log('h1 element:', h1);
+console.dir(h1);
+
+var explain = document.querySelector('#explanation');
+console.log('p id:', explain);
+console.dir(explain);
+
+var hint = document.querySelector('.hint');
+console.log('p hint:', hint);
+console.dir(hint);
+
+var p = document.querySelectorAll('p');
+console.log('all p elements:', p);
+
+var example = document.querySelectorAll('.example-link');
+console.log('all class of example-link:', example);
