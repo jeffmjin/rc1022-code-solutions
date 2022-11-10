d362196c7c03c788017fb8f484b3cffa703c4735
Jeff Jin <jeffmjin@gmail.com>
2022-11-10 18:23:19 +0000

    Finished index and css portion.

---
+++ javascript-carousel/main.js
@@ -0,0 +1,8 @@
+/* var $images = document.querySelectorAll('img');
+var $circle = document.querySelectorAll('.fa-circle');
+var $left = document.querySelector('.fa-chevron-left');
+var $right = document.querySelector('.fa-chevron-right');
+var $allCircles = document.querySelector('.circle');
+
+var count = 0;
+var intervalId; */
