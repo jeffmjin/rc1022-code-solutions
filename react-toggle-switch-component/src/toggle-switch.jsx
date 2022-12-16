9cc164036699ef2eefa343f0565f04b85ee97aa3
Jeff Jin <jeffmjin@gmail.com>
2022-12-16 17:39:22 +0000

    Not finished challenged.

---
+++ react-toggle-switch-component/src/toggle-switch.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export class ToggleSwitch extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  handleToggle(props) {
+
+  }
+
+  render() {
+
+  }
+}
